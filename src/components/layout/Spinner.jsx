183d47ad9d4f8431a12d4031a04d4d39dafb7282
Baran Sülü <73203587+Barannsulu@users.noreply.github.com>
2022-01-17 13:31:23 +0300

    Add files via upload

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,17 @@
+import spinner from './assets/spinner.gif'
+
+function Spinner() {
+    return (
+        <div className='w-100 mt-20'>
+            <img
+                src="{spinner}" alt=""
+                width={180}
+                className='text-center mx-auto'
+                src={spinner}
+                alt='Loading...'
+            />
+        </div>
+    )
+}
+
+export default Spinner
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} 
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+}
